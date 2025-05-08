70e64bcd362ce49be46bc249b220be582bc9a16e
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-05-08 10:10:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def extraer_texto(pdf_bytes):
 texto_pdf = ""
 
 if opcion == "Desde GitHub":
-    url = st.text_input("Pega la URL RAW del PDF en GitHub:", "https://raw.githubusercontent.com/inefable12/comandos_basicos_linux/src/main/LinuxCommandReferenceFOSSwire.pdf")
+    url = st.text_input("Pega la URL RAW del PDF en GitHub:", "https://raw.githubusercontent.com/inefable12/comandos_basicos_linux/main/LinuxCommandReferenceFOSSwire.pdf")
     if url:
         try:
             response = requests.get(url)
